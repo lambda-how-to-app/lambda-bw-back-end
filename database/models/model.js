46d07bb58d441ff65c9716d540c0e1f017d845e7
PascalUlor <pascalulor@yahoo.com>
2019-07-29 17:16:12 +0100

    complete user sign up test user database create token on sign up

--- database/models/model.js
+++ database/models/model.js
@@ -8,7 +8,6 @@ const findAuthUser = () => {
 };
 const findSingleUser = async filter => {
   const user = await db('authenticatedusers').where(filter);
-  console.log(user);
   return [{ ...user, guide: !!user.guide }];
 };
 const addUser = user => {
