40f90d3b6e750774d3390e83f5faa2d5b341ac40
PascalUlor <pascalulor@yahoo.com>
2019-07-30 06:45:06 +0100

    edite profile tables replace phone with profileimage

--- database/mock/guides.js
+++ database/mock/guides.js
@@ -3,151 +3,175 @@ const guides = [
     fullname: 'Cordy Skala',
     auth_id: 12,
     location_id: 12,
-    phone: '+31 516 808 2076'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Averill Giddons',
     auth_id: 2,
     location_id: 28,
-    phone: '+1 275 513 5399'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Shayne Beecroft',
     auth_id: 10,
     location_id: 15,
-    phone: '+7 902 103 5813'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Carola Simons',
     auth_id: 2,
     location_id: 39,
-    phone: '+86 548 511 0403'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Jobey De Pietri',
     auth_id: 13,
     location_id: 13,
-    phone: '+66 180 661 9516'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Cordelia Trevaskus',
     auth_id: 6,
     location_id: 13,
-    phone: '+63 783 332 3334'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Eden Laight',
     auth_id: 12,
     location_id: 13,
-    phone: '+351 493 820 4482'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Abramo Laugier',
     auth_id: 2,
     location_id: 24,
-    phone: '+86 312 533 4409'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Dallis Marconi',
     auth_id: 26,
     location_id: 28,
-    phone: '+420 426 734 7129'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Kania Case',
     auth_id: 26,
     location_id: 26,
-    phone: '+66 450 926 3458'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Ase Pattullo',
     auth_id: 39,
     location_id: 24,
-    phone: '+856 822 670 1458'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Robyn Davidowich',
     auth_id: 39,
     location_id: 29,
-    phone: '+33 114 404 4557'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Keary McErlaine',
     auth_id: 7,
     location_id: 23,
-    phone: null
+    profileimage: null
   },
   {
     fullname: 'Kristina Spinetti',
     auth_id: 35,
     location_id: 7,
-    phone: '+82 497 634 6960'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Malissa Phelp',
     auth_id: 33,
     location_id: 7,
-    phone: '+57 915 673 8360'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Izabel Copcote',
     auth_id: 9,
     location_id: 34,
-    phone: '+420 412 345 6216'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Pen Lehemann',
     auth_id: 17,
     location_id: 1,
-    phone: '+374 734 343 7946'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Kimberly Gossop',
     auth_id: 39,
     location_id: 24,
-    phone: '+86 754 490 5523'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Sue Thuillier',
     auth_id: 24,
     location_id: 11,
-    phone: '+358 887 650 6882'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Bernie Sherrington',
     auth_id: 40,
     location_id: 1,
-    phone: '+63 108 951 2388'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Patsy Gorst',
     auth_id: 18,
     location_id: 9,
-    phone: '+62 128 539 5992'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Jone Galvin',
     auth_id: 3,
     location_id: 13,
-    phone: '+993 530 697 3322'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Renie Baughn',
     auth_id: 2,
     location_id: 33,
-    phone: '+36 949 614 8677'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Reiko Callacher',
     auth_id: 7,
     location_id: 28,
-    phone: '+7 952 822 1835'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Bibbye Legging',
     auth_id: 7,
     location_id: 17,
-    phone: '+86 697 649 7778'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   }
 ];
 
--- database/mock/users.js
+++ database/mock/users.js
@@ -3,91 +3,105 @@ const users = [
     fullname: 'Martainn McCreedy',
     auth_id: 19,
     location_id: 2,
-    phone: '+995 296 507 3239'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Othilie Ebdin',
     auth_id: 36,
     location_id: 7,
-    phone: '+30 793 116 4874'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Cari Grigorescu',
     auth_id: 21,
     location_id: 3,
-    phone: '+1 618 206 7042'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Dulciana Wipper',
     auth_id: 8,
     location_id: 15,
-    phone: '+56 838 175 2982'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Woodman Edmondson',
     auth_id: 38,
     location_id: 10,
-    phone: '+355 525 622 7638'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Esteban Ong',
     auth_id: 20,
     location_id: 40,
-    phone: '+86 269 150 3080'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Ileane Ocheltree',
     auth_id: 25,
     location_id: 15,
-    phone: '+63 663 642 6065'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Rainer McShirie',
     auth_id: 36,
     location_id: 15,
-    phone: '+39 252 176 0685'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Saleem Profit',
     auth_id: 27,
     location_id: 18,
-    phone: '+30 989 627 9826'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Maude Drynan',
     auth_id: 21,
     location_id: 2,
-    phone: '+86 925 822 1156'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Elisabeth Gigg',
     auth_id: 5,
     location_id: 29,
-    phone: '+850 734 106 1060'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Karoline Allgood',
     auth_id: 20,
     location_id: 2,
-    phone: '+86 807 267 7225'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Berti Greguol',
     auth_id: 36,
     location_id: 6,
-    phone: '+86 818 493 8951'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   },
   {
     fullname: 'Nikola Petrov',
     auth_id: 19,
     location_id: 3,
-    phone: null
+    profileimage: null
   },
   {
     fullname: 'Loreen Audsley',
     auth_id: 31,
     location_id: 40,
-    phone: '+54 602 969 2307'
+    profileimage:
+      'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
   }
 ];
 
