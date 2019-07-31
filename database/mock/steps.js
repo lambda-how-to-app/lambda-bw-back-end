89ef5b2e8b628ac5037ee33e80afc72c8eb44720
PascalUlor <pascalulor@yahoo.com>
2019-07-31 02:20:46 +0100

    create database tables for lifehacks and hacksteps seed dummy data

---
+++ database/mock/steps.js
@@ -0,0 +1,129 @@
+const steps = [
+  {
+    hack_id: 10,
+    steps:
+      'Compatible What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 8,
+    steps:
+      'Triple-buffered What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 6,
+    steps:
+      'Vue Graphic Interface What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 9,
+    steps:
+      'Reverse-engineered What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 7,
+    steps:
+      'encompassing What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 10,
+    steps:
+      'exuding What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 2,
+    steps:
+      'logistical What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 5,
+    steps:
+      'tertiary What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 8,
+    steps:
+      'interactive App What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 4,
+    steps:
+      'core What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 9,
+    steps:
+      'attitude What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 5,
+    steps:
+      'radical What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 3,
+    steps:
+      'conglomeration What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 6,
+    steps:
+      'Universal What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 3,
+    steps:
+      'Reverse-engineered AI What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 5,
+    steps:
+      'Decentralized What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 6,
+    steps:
+      'knowledge user What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 10,
+    steps:
+      'optimizing What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 1,
+    steps:
+      'Graphic Interface What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 3,
+    steps:
+      'Devolved What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 6,
+    steps:
+      'eco-centric What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 4,
+    steps:
+      'Robust What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 8,
+    steps:
+      'React What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 10,
+    steps:
+      'Angular What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  },
+  {
+    hack_id: 5,
+    steps:
+      'migration What problem does your app solve? Be as specific as possible; how does your app solve the problem? What is the mission statement'
+  }
+];
+
+module.exports = steps;
