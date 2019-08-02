05f83e3a445ec03311a1cb7ccf71bf543fa83ae1
PascalUlor <pascalulor@yahoo.com>
2019-08-02 12:38:12 +0100

    add swagger api documentation

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('../routes/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../ApiDoc.json');
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.use('/api/v1', routes);
 
